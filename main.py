250679217e924b237d05c10caafe10faf02eb1fc
lpozo1990 <lpozo1990@gmail.com>
2024-01-18 12:26:49 -0500

    Using Path & Query Parameters Simultaneously

--- main.py
+++ main.py
@@ -16,3 +16,8 @@ def profile(username):
 @app.get("/products")
 def products(id=1, price=200):
     return {f"product with an id: {id} and ${price}"}
+
+
+@app.get("/profile/{userid}/comments")
+def profile(userid: int, commentid: int = 12):
+    return {f"Profile page for user {userid} and commentid {commentid}"}
