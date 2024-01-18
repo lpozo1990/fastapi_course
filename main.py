fb237e7f6849559a8d99ab50e6ad7ed0c6211274
lpozo1990 <lpozo1990@gmail.com>
2024-01-18 12:00:48 -0500

    1. Path Parameters

--- main.py
+++ main.py
@@ -9,10 +9,15 @@ def index():
 
 
 @app.get("/property/{id}")
-def property(id):
+def property(id: int):
     return f"this is a property page {id}"
 
 
 @app.get("/movies")
 def movies():
     return {"movi list": {"movie1", "movie2"}}
+
+
+@app.get("/profile/{username}")
+def profile(username: str):
+    return {f"this is the page for user {username}"}
