4015ac7af7b79a610ce973f2a895765f5386139a
lpozo1990 <lpozo1990@gmail.com>
2024-01-18 12:46:52 -0500

    Using Model Inside A Function

--- main.py
+++ main.py
@@ -10,6 +10,19 @@ class Profile(BaseModel):
     age: int = 29
 
 
+class Product(BaseModel):
+    name: str
+    price: int
+    discount: int
+    discounted_price: float
+
+
 @app.post("/adduser")
 def addUser(user: Profile):
     return {f"user data:{user.model_dump()}"}
+
+
+@app.post("/addproduct")
+def add_product(product: Product):
+    product.discounted_price = product.price - (product.price * product.discount) / 100
+    return product
